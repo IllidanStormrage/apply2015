658134fe3cee326aab122ecfa9e0c8b23406bebf
Lich2013 <longzy2013@gmail.com>
2015-07-22 19:46:15 +0800

    init

---
+++ app/Aparment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Aparment extends Model
+{
+    //
+    protected $table = 'aparment';
+
+}
---
+++ app/Http/Middleware/TokenCheck.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class TokenCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $request->input('token');
+        return $next($request);
+    }
+}
