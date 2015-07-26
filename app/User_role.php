8d3f62525d9b1f0f0c62002900ac51049e8ef45c
Lich2013 <longzy2013@gmail.com>
2015-07-26 15:28:02 +0800

    namesapce

--- app/User_role.php
+++ app/User_role.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace App;
-
+namespace App\Model;
+use App\Model\Department;
 use Illuminate\Database\Eloquent\Model;
 
 class User_role extends Model
 {
     //
     protected $table = 'user_role';
+
+    public function department() {
+        return $this->hasOne('App\Model\Department', 'id', 'department_id');
+    }
 
 }
