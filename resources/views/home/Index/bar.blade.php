f9bf6c8e397dc5bd62f05866eeb810a963c5dcdd
欧亚非 <Grallen@Grallens-MBP.lan>
2015-07-27 21:46:17 +0800

    grallen update about menu

---
+++ resources/views/home/Index/bar.blade.php
@@ -0,0 +1,68 @@
+    @extends('home.layout.container')
+
+    @section('container')
+
+    <div id="page-wrapper">
+        <div class="page-content">
+            <div class="row">
+                <div class="col-lg-12">
+                   <div class="portlet box portlet-green">
+                       <div class="portlet-header">
+                           <div class="caption">红岩网校web研发部新生报名审核进度条</div>
+                       </div>
+                       <div class="portlet-body">
+                           <div id="rootwizard-custom-circle">
+                               <div class="navbar">
+                                   <div class="navbar-inner">
+                                       <ul>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-user"></i><p class="description">笔试</p></a>
+                                           </li>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-send mln"></i><p class="description">第一次面试</p></a>
+                                           </li>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-shopping-cart"></i><p class="description">第二次面试</p></a>
+                                           </li>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-check"></i><p class="description">第三次面试</p></a>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
+                               <div id="bar" class="progress active">
+                                   <div class="bar progress-bar progress-bar-primary" style="width:25%;"></div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                   <div class="portlet box portlet-green">
+                       <div class="portlet-header">
+                           <div class="caption">红岩网校web研发部新生报名审核进度条</div>
+                       </div>
+                       <div class="portlet-body">
+                           <div id="rootwizard-custom-circle">
+                               <div class="navbar">
+                                   <div class="navbar-inner">
+                                       <ul>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-user"></i><p class="description">笔试</p></a>
+                                           </li>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-send mln"></i><p class="description">第一次面试</p></a>
+                                           </li>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-shopping-cart"></i><p class="description">第二次面试</p></a>
+                                           </li>
+                                           <li><a href="#" data-toggle="tab"><i class="glyphicon glyphicon-check"></i><p class="description">第三次面试</p></a>
+                                           </li>
+                                       </ul>
+                                   </div>
+                               </div>
+                               <div id="bar" class="progress active">
+                                   <div class="bar progress-bar progress-bar-primary" style="width:15%;"></div>
+                               </div>
+                           </div>
+                       </div>
+                   </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @stop
+
+
--- resources/views/home/Index/presidium.blade.php
+++ resources/views/home/Index/presidium.blade.php
@@ -3,7 +3,6 @@
     @section('container')
 
         <div id="page-wrapper">
-
             <div class="page-content">
                 <div class="row">
                     <div class="col-lg-12">
@@ -29,7 +28,7 @@
                                                 </thead>
                                                 <tbody>
                                                     <tr>
-                                                        <td>红岩网校站长</td>
+                                                        <td>红岩网校工作站主席</td>
                                                         <td>2013210000</td>
                                                         <td>咬不碎</td>
                                                         <td>男</td>
@@ -38,7 +37,7 @@
                                                         <td></td>
                                                     </tr>
                                                     <tr>
-                                                        <td>红岩网校副站长</td>
+                                                        <td>红岩网校工作站副主席</td>
                                                         <td>2013210000</td>
                                                         <td>龙妹</td>
                                                         <td>男</td>
@@ -47,32 +46,82 @@
                                                         <td><span class="label label-sm label-primary">删除</span></td>
                                                     </tr>
                                                     <tr>
-                                                        <td>红岩网校副站长</td>
+                                                        <td>红岩网校工作站副主席</td>
                                                         <td>2013210000</td>
                                                         <td>龙妹</td>
                                                         <td>男</td>
                                                         <td>13988888888</td>
                                                         <td>妓院</td>
                                                         <td><span class="label label-sm label-primary">删除</span></td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>
-                                                            <select class="form-control">
-                                                                <option>红岩网校副站长</option>
-                                                            </select>
-                                                        </td>
-                                                        <td>
-                                                            <input id="new" type="text" placeholder="输入需要新添加人员的学号" class="form-control" style="height: 31px;font-size: 12px;-webkit-border-radius:4px !important;-moz-border-radius: 4px !important;border-radius: 4px !important">
-                                                        </td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
-                                                        <td></td>
                                                     </tr>
                                                 </tbody>
                                             </table>
                                         </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <button type="button" data-target="#modal-add-people" data-toggle="modal" class="btn btn-default" style="float: right;background-color: #fff;">人员添加</button>
+                        <div id="modal-add-people" tabindex="-1" role="dialog" aria-labelledby="modal-prompt-label" aria-hidden="true" class="modal fade" style="display: none;">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" data-dismiss="modal" aria-hidden="true" class="close">×</button>
+                                        <h4 id="modal-prompt-label" class="modal-title">新人员添加</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form action="#" class="form-horizontal">
+                                            <div class="form-body pal">
+                                                <div class="form-group">
+                                                    <label for="presidium" class="col-md-3 control-label">组织</label>
+                                                    <div class="col-md-9">
+                                                        <div class="input-icon right">
+                                                            <select class="form-control" id="presidium" style="height: 31px;font-size: 12px;">
+                                                                <option>红岩网校工作站</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="job" class="col-md-3 control-label">职位</label>
+                                                    <div class="col-md-9">
+                                                        <div class="input-icon right">
+                                                            <select class="form-control" id="job" style="height: 31px;font-size: 12px;">
+                                                                <option>副主席</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="studentID" class="col-md-3 control-label">学号</label>
+                                                    <div class="col-md-9">
+                                                        <div class="input-icon right">
+                                                            <input id="studentID" type="text" placeholder="请输入需要新添加人员的学号" class="form-control" style="height: 31px;font-size: 12px;-webkit-border-radius:4px !important;-moz-border-radius: 4px !important;border-radius: 4px !important">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <p class="col-md-3 control-label">姓名</p>
+                                                    <p class="col-md-9 control-label" style="text-align: left">隆综艺</p>
+                                                </div>
+                                                <div class="form-group">
+                                                    <p class="col-md-3 control-label">性别</p>
+                                                    <p class="col-md-9 control-label" style="text-align: left">男</p>
+                                                </div>
+                                                <div class="form-group">
+                                                    <p class="col-md-3 control-label">手机号</p>
+                                                    <p class="col-md-9 control-label" style="text-align: left">13888888888</p>
+                                                </div>
+                                                <div class="form-group">
+                                                    <p class="col-md-3 control-label">学院</p>
+                                                    <p class="col-md-9 control-label" style="text-align: left">妓院</p>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" data-dismiss="modal" class="btn btn-default">取消</button>
+                                        <button type="button" class="btn btn-primary">确认</button>
                                     </div>
                                 </div>
                             </div>
